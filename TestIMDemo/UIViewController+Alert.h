893d4628481bfc773cd53f6c7bc8853160df5bcf
任波 <borenfocus@gmail.com>
2017-07-25 11:25:14 +0800

    主动添加好友和接收好友的添加请求

---
+++ TestIMDemo/UIViewController+Alert.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Alert.h
+//  TestIMDemo
+//
+//  Created by 任波 on 2017/7/25.
+//  Copyright © 2017年 renb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Alert)
+
+@end
---
+++ TestIMDemo/UIViewController+Alert.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Alert.m
+//  TestIMDemo
+//
+//  Created by 任波 on 2017/7/25.
+//  Copyright © 2017年 renb. All rights reserved.
+//
+
+#import "UIViewController+Alert.h"
+
+@implementation UIViewController (Alert)
+
+@end
